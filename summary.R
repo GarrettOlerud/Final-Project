2006b1299eb56b484d9b698baad133450d4c3e97
Madison Hess <hessmad@uw.edu>
2018-05-14 17:08:27 -0700

    Add source

--- summary.R
+++ summary.R
@@ -1,6 +1,8 @@
 # Set working directory then read in relevant libraries and data 
 library("dplyr")
 pet_data <- read.csv("data/seattle_pet_licenses.csv", stringsAsFactors = FALSE)
-tax_data <- read.delim("data/wa_incomes_zip_code.csv", stringsAsFactors = FALSE)
+tax_data <- read.delim("data/wa_incomes_zip_code.csv", 
+                       stringsAsFactors = FALSE) # S/O to Adele for rescuing us
+# from this _tab_ seperated file.
 
 
