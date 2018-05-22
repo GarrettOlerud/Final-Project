f1c8dfaeca50a1c7bcfe983288b83f5a9bc147f8
SpencerJKnapp <knapps@uw.edu>
2018-05-21 19:50:24 -0700

    create GeneralNotes md file

--- summary.R
+++ summary.R
@@ -2,7 +2,7 @@
 library("dplyr")
 pet_data <- read.csv("data/seattle_pet_licenses.csv", stringsAsFactors = FALSE)
 tax_data <- read.delim("data/wa_incomes_zip_code.csv", 
-                       stringsAsFactors = FALSE) # S/O to Adele for rescuing us
+                       stringsAsFactors = FALSE) 
+# S/O to Adele for rescuing us
 # from this _tab_ seperated file.
-
 
