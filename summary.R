5dacb478ca874d44a3ca07eda02f3068a09452a2
SpencerJKnapp <knapps@uw.edu>
2018-05-21 20:30:16 -0700

    work on data wrangling/GeneralNotes

--- summary.R
+++ summary.R
@@ -1,8 +1,17 @@
 # Set working directory then read in relevant libraries and data 
-library("dplyr")
+install.packages("zipcode")
+install.packages("dplyr")
+library(zipcode)
+library(dplyr)
 pet_data <- read.csv("data/seattle_pet_licenses.csv", stringsAsFactors = FALSE)
 tax_data <- read.delim("data/wa_incomes_zip_code.csv", 
                        stringsAsFactors = FALSE) 
 # S/O to Adele for rescuing us
 # from this _tab_ seperated file.
 
+pet_data_zip <- pet_data %>% group_by(zip_code)
+
+joined_data <- left_join(pet_data, tax_data, by = )
+
+#Recommend using tax bracket bins and now joining data sets
+#Don't join dataframes if it makes no sense
