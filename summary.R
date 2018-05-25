bb4dc24b336a8d8de1fa4a62d5794ac3b4b412c1
SpencerJKnapp <knapps@uw.edu>
2018-05-24 19:33:31 -0700

    wrangle pet data

--- summary.R
+++ summary.R
@@ -1,8 +1,8 @@
 # Set working directory then read in relevant libraries and data 
-
-library("dplyr")
-library("maps")
-library("zipcode")
+library(dplyr)
+library(maps)
+library(zipcode)
+data(zipcode)
 
 pet_data <- read.csv("data/seattle_pet_licenses.csv", stringsAsFactors = FALSE)
 tax_data <- read.delim("data/wa_incomes_zip_code.csv", 
@@ -21,17 +21,31 @@ joined_data <- left_join(pet_data, tax_data, by = "")
 
 #Recommend using tax bracket bins and now joining data sets
 #Don't join dataframes if it makes no sense
-#attach lat and long
+
+#Attach lat and long using zipcode package
 colnames(pet_data)[which(names(pet_data) == "zip_code")] <- "zip"
 colnames(zipcode)[which(names(zipcode) == "zip_code")] <- "zip"
 colnames(tax_data)[which(names(tax_data) == "Zip.Code")] <- "zip"
 pet_data_with_lat <- left_join(pet_data, zipcode, by = "zip")
+
 #tax_data_clean <- clean.zipcodes(tax_data)
 #tax_with_lat <- left_join(zipcode, tax_data)
 
+#Working on most_adopted_pets visualization below
+most_adopted <- pet_data_with_lat %>% select(primary_breed,
+                                              species, zip, city, latitude, longitude)
+#Function from David Arenburg
+#https://tinyurl.com/yafunvtp
+
+freqfunc <- function(x, n){
+  tail(sort(table(unlist(strsplit(as.character(x), ", ")))), n)
+}
+
+top_5_dogs <- freqfunc(most_adopted$primary_breed, 5)
+top_5_dogs_df <- data.frame(top_5_dogs) %>% arrange(desc(Freq))
 
 
+most_adopted_cats <- most_adopted %>% filter(species == "Cat")
 
-
-
-
+top_5_cats <- freqfunc(most_adopted_cats$primary_breed, 5)
+top_5_cats_df <- data.frame(top_5_cats) %>% arrange(desc(Freq))
