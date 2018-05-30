bd7d81c40e6ebfdab8d7bf6f0eb062fe56764116
Garrett Olerud <golerud701@gmail.com>
2018-05-29 23:52:56 -0700

    linted finaly

--- ui.R
+++ ui.R
@@ -7,12 +7,12 @@ library(ggmap)
 library(DT)
 library(shinythemes)
 source("summary.R")
-#Read in Pet data
+# Read in Pet data
 pet_data <- read.csv("data/seattle_pet_licenses.csv",
   stringsAsFactors = FALSE,
   na.strings = c("", " ", "NA", "   ", "     ")
 )
-#Read in Tax data
+# Read in Tax data
 tax_data <- read.delim("data/wa_incomes_zip_code.csv",
   stringsAsFactors = FALSE
 ) # S/O to Adele for rescuing us
@@ -21,32 +21,39 @@ tax_data <- read.delim("data/wa_incomes_zip_code.csv",
 # Begin UI
 
 ui <- navbarPage(
- #set theme for entire app
-   theme = shinytheme("sandstone"),
+  # set theme for entire app
+  theme = shinytheme("sandstone"),
   # tab 1
   "Final",
-#first tab
+  # first tab
   tabPanel(
     titlePanel("Overview"),
     mainPanel(
       "Welcome to the spring 2018 Info 201 final project by Madi Hess,
-              Spencer Knapp, and Garrett Olerud.  The data used in this project comes from",
-      a("Link to Data", href = "https://www.kaggle.com/aaronschlegel/seattle-pet-licenses/version/1"),
-      " and includes information on the zipcodes of pet adoptions and the primary and secondary breed of
-              the dog or cat adopted.  There is also information about the tax brackets and how many people fall into
-              each of the brackets and filing statuses by zipcode.  The goal of this project is to discover the relationships 
-              between the frequency of pet adoptions and the wealth of the area."
+              Spencer Knapp, and Garrett Olerud.
+      The data used in this project comes from",
+      a("Link to Data",
+      href = "https://www.kaggle.com/aaronschlegel/
+      seattle-pet-licenses/version/1"),
+      " and includes information on the zipcodes of pet adoptions and
+      the primary and secondary breed of the dog or cat adopted.  There
+      is also information about the tax brackets and how many people
+      fall into each of the brackets and filing statuses by zipcode.
+      The goal of this project is to discover the relationships
+      between the frequency of pet adoptions and the wealth of the area."
     )
   ),
-#second tab  
+  # second tab
   tabPanel(
     titlePanel("Pet Adoptions by Zipcode"),
     headerPanel("Select"),
-    sidebarPanel(("Use the dropdown selector to choose the
+    sidebarPanel( ("Use the dropdown selector to choose the
                   the data you would like to see plotted.
                   Each option plots the pet
-                  adoptions from 2005 - 2017 using zipcode
-                  for either: All Pets, Cats, or Dogs.")),
+                  adoptions from 2005 -
+                  2017 using zipcode
+                  for either: All Pets,
+                  Cats, or Dogs.")),
     # create tabs to select data to be plotted
     tabsetPanel(
       type = "tabs",
@@ -83,7 +90,7 @@ ui <- navbarPage(
                  search bar to filter down resuts even more
                  say you want to find how many Terriers there
                  are in a seattle zipcode your would type Terrier, 98107"),
-    #Create tabs to select what data you want displayed 
+    # Create tabs to select what data you want displayed
     tabsetPanel(
       type = "tabs",
       tabPanel("Top Cats", DT::dataTableOutput("top_5_cats_df")),
@@ -97,11 +104,13 @@ ui <- navbarPage(
     headerPanel("something insightful"),
     sidebarPanel(
       checkboxGroupInput("work",
-                         label = "please",
-                         choices = brackets_adoptions$Zip,
-                         selected = NULL)),
+        label = "please",
+        choices = brackets_adoptions$Zip,
+        selected = NULL
+      )
+    ),
     mainPanel(
       plotOutput("plot_44")
-    ))
+    )
+  )
 )
-
